68bee362b203f4d8d96bd1ee53bc4317013af34b
Delphin Habierre <dhabierre@gmail.com>
2019-02-13 14:19:10 +0100

    Initial import

---
+++ DhaDevBox.ps1
@@ -0,0 +1,9 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Install Software
+choco install -y visualstudiocode
+choco install -y git
+choco install -y sourcetree
+choco install -y nodejs-lts
+choco install -y winmerge
